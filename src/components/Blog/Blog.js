6f1916864f4a3bcb41f03804127818bcd19543c6
mehedihasankhairul <mhkhairul78@gmail.com>
2021-07-01 01:33:38 +0600

    auto reader and comment added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -14,7 +14,9 @@ const Blog = ({ blog: { _id, image, name, description } }) => {
     const date = new Date;
     const formattedDate = format(date, "MMMM do, yyyy H:mma");
 
-    console.log(formattedDate);
+    const randomReader = Math.floor(10 + Math.random() * 90);
+    const randomComment = Math.floor(10 + Math.random() * 90);
+
 
     return (
 
@@ -34,7 +36,7 @@ const Blog = ({ blog: { _id, image, name, description } }) => {
 
                         {/**Description*/}
                         <p className="leading-relaxed whitespace-normal">
-                            {description.substr(0, 150)+'...'}
+                            {description.substr(0, 150) + '...'}
                         </p>
 
                         <div href="/blog/_id" className="flex items-center flex-wrap">
@@ -50,7 +52,7 @@ const Blog = ({ blog: { _id, image, name, description } }) => {
                                     stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24 ">
                                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                     <circle cx="12" cy="12" r="3">  </circle>
-                                </svg>1.0K
+                                </svg>{randomReader}K
                             </span>
                             <span className="text-gray-400 inline-flex items-center leading-none text-sm">
                                 <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
@@ -58,7 +60,7 @@ const Blog = ({ blog: { _id, image, name, description } }) => {
                                     <path
                                         d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
                                     </path>
-                                </svg>6
+                                </svg>{randomComment}
                             </span>
                         </div>
                     </div>
